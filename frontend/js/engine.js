4de9325744a62eff04d940f184baa38eb560fefa
Karl0259 <karldiril6@gmail.com>
2026-05-01 22:08:35 +0200

    refactor: clean up typing engine logic

--- frontend/js/engine.js
+++ frontend/js/engine.js
@@ -1,18 +1,19 @@
 let jeu = document.querySelectorAll("span");
 
-
 let i = 0;
+jeu[i].classList.add("actif");
 
 document.body.addEventListener("keydown", (input) => {
-    if (input.key.length == 1){
-        if (jeu[i].textContent == input.key) {
-        console.log("Oui")
-        jeu[i].classList.add('bon');
-    }
-    else {
-        console.log("Non");
-        jeu[i].classList.add('erreur');
-    }
-    i += 1
+    console.log("test");
+    if (input.key.length == 1) {
+        if (input.key == jeu[i].textContent) {
+            jeu[i].classList.add("correct");
+        }
+        else {
+            jeu[i].classList.add("incorrect");
+        }
+        jeu[i].classList.remove("actif");
+        ++i;
+        
     }
 })
