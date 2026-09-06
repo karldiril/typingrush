e66f3d4447a72440e356b12eca9ba15d4767ba57
Karl0259 <karldiril6@gmail.com>
2026-09-06 19:09:25 +0200

    feat: implement user registration in AuthService

---
+++ backend/Services/AuthService.cs
@@ -0,0 +1,67 @@
+namespace backend.Services;
+
+using backend.DTOs;
+using backend.Models;
+using backend.Data;
+using Microsoft.EntityFrameworkCore;
+
+public class AuthService : IAuthService
+{
+
+    private readonly TypovelDbContext _context;
+
+    public AuthService(TypovelDbContext context)
+    {
+        _context = context;
+    }
+
+
+    public async Task<User?> RegisterAsync(RegisterDto registerDto)
+    {
+        if (await AlreadyUsedEmailAsync(registerDto.Email) || await AlreadyUsedPseudoAsync(registerDto.Pseudo))
+        {
+            return null;
+        }
+
+        string passwordHash = HasherPassword(registerDto.Password);
+        var newUser = new User
+
+        {
+            Pseudo = registerDto.Pseudo.Trim(),
+            Email = registerDto.Email.Trim().ToLower(),
+            HashPassword = passwordHash,
+            CreatedAt = DateTimeOffset.UtcNow
+        };       
+
+        _context.Users.Add(newUser);
+        await _context.SaveChangesAsync();
+
+        return newUser;
+    }
+
+    public async Task<string?> LoginAsync(LoginDto loginDto)
+    {
+        
+        throw new NotImplementedException();
+    }
+
+    private async Task<bool> AlreadyUsedEmailAsync(string email)
+    {
+        string lowerEmail = email.Trim().ToLower();
+        bool isAlreadyUsed = await _context.Users.AnyAsync(u => u.Email.ToLower() == lowerEmail);
+        return isAlreadyUsed;
+    }
+
+    private async Task<bool> AlreadyUsedPseudoAsync(string pseudo)
+    {
+        string lowerPseudo = pseudo.Trim().ToLower();
+        bool isAlreadyUsed = await _context.Users.AnyAsync(u => u.Pseudo.ToLower() == lowerPseudo);
+        return isAlreadyUsed;
+    }
+
+    private string HasherPassword(string password)
+    {
+        string passwordHash = BCrypt.Net.BCrypt.HashPassword(password);
+        return passwordHash;
+    }
+}
